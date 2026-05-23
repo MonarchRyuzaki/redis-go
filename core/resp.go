cddb81d17d6e5c3350c6eaf164066d9453cbf62d
MonarchRyuzaki <shivamganguly2004@gmail.com>
2026-05-23 18:13:30 +0530

    resp implementation

--- core/resp.go
+++ core/resp.go
@@ -1,1 +1,194 @@
 package core
+
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+)
+
+const (
+	STRING     = '+'
+	ERROR      = '-'
+	INTEGER    = ':'
+	BULK       = '$'
+	ARRAY      = '*'
+	NULL_BULK  = 'n'
+	NULL_ARRAY = 'N'
+	RDB_FILE   = 'R'
+	STREAMS    = 'S'
+)
+
+type Value struct {
+	Type  byte
+	Str   string
+	Num   int
+	Bulk  string
+	Array []Value
+}
+
+func Marshal(v Value) []byte {
+	switch v.Type {
+	case STRING:
+		return []byte(fmt.Sprintf("+%s\r\n", v.Str))
+	case ERROR:
+		return []byte(fmt.Sprintf("-%s\r\n", v.Str))
+	case INTEGER:
+		return []byte(fmt.Sprintf(":%d\r\n", v.Num))
+	case BULK:
+		return []byte(fmt.Sprintf("$%d\r\n%s\r\n", len(v.Bulk), v.Bulk))
+	case ARRAY:
+		var res []byte
+		res = append(res, []byte(fmt.Sprintf("*%d\r\n", len(v.Array)))...)
+		for _, val := range v.Array {
+			res = append(res, Marshal(val)...)
+		}
+		return res
+	case NULL_BULK:
+		return []byte("$-1\r\n")
+	case NULL_ARRAY:
+		return []byte("*-1\r\n")
+	case RDB_FILE:
+		header := fmt.Sprintf("$%d\r\n", len(v.Bulk))
+		return append([]byte(header), []byte(v.Bulk)...)
+	case STREAMS:
+		var res []byte
+		for _, val := range v.Array {
+			res = append(res, Marshal(val)...)
+		}
+		return res
+	default:
+		return nil
+	}
+}
+
+func Unmarshal(data []byte) (Value, int, error) {
+	if len(data) == 0 {
+		return Value{}, 0, fmt.Errorf("empty data")
+	}
+
+	_type := data[0]
+	switch _type {
+	case STRING:
+		return unmarshalSimpleString(data)
+	case ERROR:
+		return unmarshalError(data)
+	case INTEGER:
+		return unmarshalInt(data)
+	case BULK:
+		return unmarshalBulk(data)
+	case ARRAY:
+		return unmarshalArray(data)
+	default:
+		// Inline command (treat as array of bulks)
+		return unmarshalInline(data)
+	}
+}
+
+func readLine(data []byte) ([]byte, int, error) {
+	for i := 0; i < len(data)-1; i++ {
+		if data[i] == '\r' && data[i+1] == '\n' {
+			return data[:i], i + 2, nil
+		}
+	}
+	return nil, 0, fmt.Errorf("no CRLF found")
+}
+
+func unmarshalSimpleString(data []byte) (Value, int, error) {
+	line, n, err := readLine(data[1:])
+	if err != nil {
+		return Value{}, 0, err
+	}
+	return Value{Type: STRING, Str: string(line)}, n + 1, nil
+}
+
+func unmarshalError(data []byte) (Value, int, error) {
+	line, n, err := readLine(data[1:])
+	if err != nil {
+		return Value{}, 0, err
+	}
+	return Value{Type: ERROR, Str: string(line)}, n + 1, nil
+}
+
+func unmarshalInt(data []byte) (Value, int, error) {
+	line, n, err := readLine(data[1:])
+	if err != nil {
+		return Value{}, 0, err
+	}
+	num, err := strconv.Atoi(string(line))
+	if err != nil {
+		return Value{}, 0, err
+	}
+	return Value{Type: INTEGER, Num: num}, n + 1, nil
+}
+
+func unmarshalBulk(data []byte) (Value, int, error) {
+	line, n, err := readLine(data[1:])
+	if err != nil {
+		return Value{}, 0, err
+	}
+	length, err := strconv.Atoi(string(line))
+	if err != nil {
+		return Value{}, 0, err
+	}
+
+	if length == -1 {
+		return Value{Type: NULL_BULK}, n + 1, nil
+	}
+
+	totalRead := n + 1
+	if len(data) < totalRead+length+2 {
+		return Value{}, 0, fmt.Errorf("not enough data for bulk string")
+	}
+
+	bulk := data[totalRead : totalRead+length]
+	if data[totalRead+length] != '\r' || data[totalRead+length+1] != '\n' {
+		return Value{}, 0, fmt.Errorf("missing CRLF after bulk string")
+	}
+
+	return Value{Type: BULK, Bulk: string(bulk)}, totalRead + length + 2, nil
+}
+
+func unmarshalArray(data []byte) (Value, int, error) {
+	line, n, err := readLine(data[1:])
+	if err != nil {
+		return Value{}, 0, err
+	}
+	length, err := strconv.Atoi(string(line))
+	if err != nil {
+		return Value{}, 0, err
+	}
+
+	if length == -1 {
+		return Value{Type: NULL_ARRAY}, n + 1, nil
+	}
+
+	res := Value{Type: ARRAY, Array: make([]Value, 0, length)}
+	totalRead := n + 1
+	for i := 0; i < length; i++ {
+		val, read, err := Unmarshal(data[totalRead:])
+		if err != nil {
+			return Value{}, 0, err
+		}
+		res.Array = append(res.Array, val)
+		totalRead += read
+	}
+
+	return res, totalRead, nil
+}
+
+func unmarshalInline(data []byte) (Value, int, error) {
+	line, n, err := readLine(data)
+	if err != nil {
+		return Value{}, 0, err
+	}
+
+	parts := bytes.Split(line, []byte(" "))
+	var array []Value
+	for _, part := range parts {
+		if len(part) > 0 {
+			array = append(array, Value{Type: BULK, Bulk: string(part)})
+		}
+	}
+
+	return Value{Type: ARRAY, Array: array}, n, nil
+}
--- core/resp_test.go
+++ core/resp_test.go
@@ -1,1 +1,102 @@
-package core
+package core_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/MonarchRyuzaki/redis-go/core"
+)
+
+func TestSimpleStringDecode(t *testing.T) {
+	cases := map[string]string{
+		"+OK\r\n": "OK",
+	}
+	for k, v := range cases {
+		value, _, _ := core.Unmarshal([]byte(k))
+		if v != value.Str {
+			t.Fail()
+		}
+	}
+}
+
+func TestError(t *testing.T) {
+	cases := map[string]string{
+		"-Error message\r\n": "Error message",
+	}
+	for k, v := range cases {
+		value, _, _ := core.Unmarshal([]byte(k))
+		if v != value.Str {
+			t.Fail()
+		}
+	}
+}
+
+func TestInt64(t *testing.T) {
+	cases := map[string]int64{
+		":0\r\n":    0,
+		":1000\r\n": 1000,
+	}
+	for k, v := range cases {
+		value, _, _ := core.Unmarshal([]byte(k))
+		if v != int64(value.Num) {
+			t.Fail()
+		}
+	}
+}
+
+func TestBulkStringDecode(t *testing.T) {
+	cases := map[string]string{
+		"$5\r\nhello\r\n": "hello",
+		"$0\r\n\r\n":      "",
+	}
+	for k, v := range cases {
+		value, _, _ := core.Unmarshal([]byte(k))
+		if v != value.Bulk {
+			t.Fail()
+		}
+	}
+}
+
+func TestArrayDecode(t *testing.T) {
+	cases := map[string][]interface{}{
+		"*0\r\n":                                                   {},
+		"*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n":                     {"hello", "world"},
+		"*3\r\n:1\r\n:2\r\n:3\r\n":                                 {int64(1), int64(2), int64(3)},
+		"*5\r\n:1\r\n:2\r\n:3\r\n:4\r\n$5\r\nhello\r\n":            {int64(1), int64(2), int64(3), int64(4), "hello"},
+		"*2\r\n*3\r\n:1\r\n:2\r\n:3\r\n*2\r\n+Hello\r\n-World\r\n": {[]interface{}{int64(1), int64(2), int64(3)}, []interface{}{"Hello", "World"}},
+	}
+
+	var toVal func(core.Value) interface{}
+	toVal = func(v core.Value) interface{} {
+		switch v.Type {
+		case core.STRING, core.ERROR:
+			return v.Str
+		case core.INTEGER:
+			return int64(v.Num)
+		case core.BULK:
+			return v.Bulk
+		case core.ARRAY:
+			var res []interface{}
+			for _, val := range v.Array {
+				res = append(res, toVal(val))
+			}
+			return res
+		}
+		return nil
+	}
+
+	for k, v := range cases {
+		value, _, _ := core.Unmarshal([]byte(k))
+		array := value.Array
+		if len(array) != len(v) {
+			t.Errorf("key %s: expected length %d, got %d", k, len(v), len(array))
+			continue
+		}
+		for i := range array {
+			got := toVal(array[i])
+			if fmt.Sprintf("%v", v[i]) != fmt.Sprintf("%v", got) {
+				t.Errorf("key %s, index %d: expected %v, got %v", k, i, v[i], got)
+			}
+		}
+	}
+}
