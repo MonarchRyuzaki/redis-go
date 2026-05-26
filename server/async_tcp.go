ab8b556d951568535709808345722e05eb126721
MonarchRyuzaki <shivamganguly2004@gmail.com>
2026-05-26 21:57:36 +0530

    implemented passive, active cleanup for keys

--- server/async_tcp.go
+++ server/async_tcp.go
@@ -4,12 +4,15 @@ import (
 	"log"
 	"net"
 	"syscall"
+	"time"
 
 	"github.com/MonarchRyuzaki/redis-go/config"
 	"github.com/MonarchRyuzaki/redis-go/core"
 )
 
 var con_clients int = 0
+var cronFrequency time.Duration = 1 * time.Second
+var lastCronExecTime time.Time = time.Now()
 
 func RunASyncTCPServer() error {
 	log.Println("starting an asynchronous TCP server on", config.Host, config.Port)
@@ -67,6 +70,11 @@ func RunASyncTCPServer() error {
 	}
 
 	for {
+		if time.Now().After(lastCronExecTime.Add(cronFrequency)) {
+			core.DeleteExpiredKeys()
+			lastCronExecTime = time.Now()
+		}
+
 		// see if any FD is ready for IO
 		nevents, e := syscall.EpollWait(epollFD, events[:], -1)
 		if e != nil {
