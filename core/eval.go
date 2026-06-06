5b529a25cfae594871e5bb1b6340f6ad9a952461
MonarchRyuzaki <shivamganguly2004@gmail.com>
2026-06-06 18:24:41 +0530

    implementing graceful shutdown

--- core/eval.go
+++ core/eval.go
@@ -41,6 +41,8 @@ func EvalAndRespond(cmds RedisCmds, c io.ReadWriter) {
 			buf.Write(evalLATENCY(cmd.Args))
 		case "LRU":
 			buf.Write(evalLRU(cmd.Args))
+		case "SLEEP":
+			buf.Write(evalSLEEP(cmd.Args))
 		default:
 			buf.Write(evalPING(cmd.Args))
 		}
@@ -245,5 +247,18 @@ func evalLATENCY(args []string) []byte {
 
 func evalLRU(args []string) []byte {
 	evictAllKeysLRU()
+	return Marshal(Value{Type: STRING, Str: "OK"})
+}
+
+func evalSLEEP(args []string) []byte {
+	if len(args) != 1 {
+		return Marshal(Value{Type: ERROR, Str: "ERR wrong number of arguments for 'SLEEP' command"})
+	}
+
+	durationSec, err := strconv.ParseInt(args[0], 10, 64)
+	if err != nil {
+		return Marshal(Value{Type: ERROR, Str: "ERR value is not an integer or out of range"})
+	}
+	time.Sleep(time.Duration(durationSec) * time.Second)
 	return Marshal(Value{Type: STRING, Str: "OK"})
 }
--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 
 	"github.com/MonarchRyuzaki/redis-go/config"
 	"github.com/MonarchRyuzaki/redis-go/server"
@@ -17,5 +21,15 @@ func setupFlags() {
 func main() {
 	setupFlags()
 	log.Println("rolling the dice 🎲")
-	server.RunASyncTCPServer()
+
+	var sigs chan os.Signal = make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGTERM, syscall.SIGINT)
+	
+	var wg sync.WaitGroup
+	wg.Add(2)
+	
+	go server.RunASyncTCPServer(&wg)
+	go server.WaitForSignal(&wg, sigs)
+	
+	wg.Wait()
 }
